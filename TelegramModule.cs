735511b5f5b15ced37219c26138d5a1b9c38c3dd
Gailoks <yra.cimonov@gmail.com>
2024-08-01 16:35:46 +0300

    Full working prototype for talking to ai

--- TelegramModule.cs
+++ TelegramModule.cs
@@ -3,28 +3,52 @@ using TelegramAIBot.Telegram;
 using TelegramAIBot.Telegram.Sequences.Conditions;
 using Telegram.Bot.Types;
 using Telegram.Bot;
+using TelegramAIBot.AI.Abstractions;
+using TGMessage = Telegram.Bot.Types.Message;
+using Message = TelegramAIBot.AI.Abstractions.Message;
+using Telegram.Bot.Types.Enums;
 
 
 namespace TelegramAIBot;
 
-internal class TelegramModule : ITelegramSequenceModule
+internal class TelegramModule(IAIClient aiClient) : ITelegramSequenceModule
 {
-        private TelegramClient? _client;
+    private readonly IAIClient _aiClient = aiClient;
+    private TelegramClient? _client;
 
 
-	private TelegramClient Client => _client ?? throw new InvalidOperationException("Bind before use");
+    private TelegramClient Client => _client ?? throw new InvalidOperationException("Bind before use");
 
 
-	public void BindClient(TelegramClient client) => _client = client;
+    public void BindClient(TelegramClient client) => _client = client;
 
 
-	[TelegramSequence(typeof(CommandCondition), "start")]
-    public async IAsyncEnumerator<WaitCondition> ProcessCommandStartAsync(Message message, SequenceTrigger trigger)
+    [TelegramSequence(typeof(CommandCondition), "start")]
+    public async IAsyncEnumerator<WaitCondition> ProcessCommandStartAsync(TGMessage message, SequenceTrigger trigger)
     {
-        await Client.Client.SendTextMessageAsync(message.Chat,"Hello");
-        TextMessageCondition dataWaitCondition = new TextMessageCondition();
-        yield return dataWaitCondition;
-        await Client.Client.SendTextMessageAsync(message.Chat,dataWaitCondition.CapturedMessage.Text!);
-        yield break;
-    }    
+        await Client.SendTextMessageAsync(message.Chat, "New session started, .exit to finish");
+        var aiChat = _aiClient.CreateChat();
+
+
+        while (true)
+        {
+            var waitCondition = new TextMessageCondition();
+            yield return waitCondition;
+            string text = waitCondition.CapturedMessage.Text!;
+
+            aiChat.Messages.Add(new Message(MessageRole.User, text));
+
+            var newMessageTask = aiChat.CreateChatCompletionAsync();
+
+            while (true)
+            {
+                await Client.SendChatActionAsync(waitCondition.CapturedMessage.Chat, ChatAction.Typing);
+                await Task.WhenAny(newMessageTask, Task.Delay(4000));
+                if (newMessageTask.IsCompleted)
+                    break;
+            }
+
+            await Client.SendTextMessageAsync(message.Chat, newMessageTask.Result.Content);
+        }
+    }
 }
