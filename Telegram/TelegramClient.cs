452100606c326bccf28e08627782110f0dd41721
LloydLion <lloydlion2211@gmail.com>
2024-08-10 01:51:26 +0300

    Telegram client synchronization

--- Telegram/TelegramClient.cs
+++ Telegram/TelegramClient.cs
@@ -1,10 +1,10 @@
 using System.Collections.Concurrent;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using TelegramAIBot.Threading;
 
 namespace TelegramAIBot.Telegram;
 
@@ -12,9 +12,12 @@ class TelegramClient(IOptions<TelegramClient.Options> options, ITelegramEventHan
 {
     public const char CommandPrefix = '/';
 
-    private readonly ConcurrentDictionary<long, SemaphoreSlim> _sync = [];
-    private readonly ITelegramEventHandler _handler = handler;
+	private readonly ConcurrentDictionary<long, SemaphoreSlim> _sync = [];
+	private readonly ITelegramEventHandler _handler = handler;
     private readonly ILogger? _logger = logger;
+	private readonly ManagedThread _thread = new("Telegram main thread");
+
+
     public void Start()
     {
         this.StartReceiving(HandleUpdateAsync, HandleExceptionAsync, new ReceiverOptions()
@@ -22,52 +25,55 @@ class TelegramClient(IOptions<TelegramClient.Options> options, ITelegramEventHan
             AllowedUpdates = [UpdateType.Message, UpdateType.CallbackQuery]
         });
 
-        _logger?.LogInformation("Bot started receiving");
+		_thread.Start();
+		ManagedThreadSynchronizationContext.UseInThread(_thread);
+
+		_logger?.LogInformation("Bot started receiving");
     }
 
     private Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        HandleInternalUpdateAsync(update);
+		_thread.EnqueueTask((update) => HandleInternalUpdateAsync((Update)update!), update);
         return Task.CompletedTask;
     }
 
     private async void HandleInternalUpdateAsync(Update update)
-    {
-        SemaphoreSlim? semaphore = null;
-        _logger?.LogTrace("An update received: {Update}", JsonConvert.SerializeObject(update));
+	{
+		SemaphoreSlim? semaphore = null;
+		_logger?.LogTrace("An update received: {Update}", JsonConvert.SerializeObject(update));
         try
         {
             switch (update.Type)
             {
                 case UpdateType.Message:
                     Message message = update.Message!;
-                    await waitSemaphoreAsync(message.Chat.Id, out semaphore);
-                    if (message.Type == MessageType.Text && message.Text!.StartsWith(CommandPrefix))
+					await waitSemaphoreAsync(message.Chat.Id, out semaphore);
+					if (message.Type == MessageType.Text && message.Text!.StartsWith(CommandPrefix))
                         await _handler.HandleCommandAsync(message.Text[1..], message);
                     else await _handler.HandleMessageAsync(message);
                     break;
                 case UpdateType.CallbackQuery:
 
                     CallbackQuery callbackQuery = update.CallbackQuery!;
-                    await waitSemaphoreAsync(callbackQuery.From.Id, out semaphore);
-                    await _handler.HandleButtonAsync(callbackQuery.Message!, callbackQuery.Data!);
+					await waitSemaphoreAsync(callbackQuery.From.Id, out semaphore);
+					await _handler.HandleButtonAsync(callbackQuery.Message!, callbackQuery.Data!);
                     break;
             }
         }
         catch (Exception ex)
         {
             _logger?.LogError(ex, "Exception during processing update");
-        }
-        finally
-        {
-            semaphore?.Release();
-        }
-        Task waitSemaphoreAsync(long chatId, out SemaphoreSlim semaphore)
-        {
-            semaphore = _sync.GetOrAdd(chatId, (_) => new SemaphoreSlim(1));
-            return semaphore.WaitAsync();
-        }
-    }
+		}
+		finally
+		{
+			semaphore?.Release();
+		}
+		Task waitSemaphoreAsync(long chatId, out SemaphoreSlim semaphore)
+		{
+			semaphore = _sync.GetOrAdd(chatId, (_) => new SemaphoreSlim(1));
+			return semaphore.WaitAsync();
+		}
+	}
 
     private Task HandleExceptionAsync(ITelegramBotClient client, Exception exception, CancellationToken ct)
     {
