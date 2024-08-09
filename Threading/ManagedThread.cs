ea57842ea0d82edd56b214fde41b740c554b897a
LloydLion <lloydlion2211@gmail.com>
2024-08-10 01:51:26 +0300

    Telegram client synchronization

---
+++ Threading/ManagedThread.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Concurrent;
+
+namespace TelegramAIBot.Threading;
+
+internal sealed class ManagedThread : IDisposable
+{
+	private readonly ConcurrentQueue<ThreadTask> _queue = new();
+	private readonly Thread _thread;
+	private readonly AutoResetEvent _enqueueEvent = new(false);
+	private bool _isDisposing = false;
+
+
+	public ManagedThread(string internalThreadName = "ManagedThread thread")
+	{
+		_thread = new Thread(ThreadWorker);
+		_thread.Name = internalThreadName;
+	}
+
+
+	public bool IsInside => Environment.CurrentManagedThreadId == _thread.ManagedThreadId;
+
+
+	public void Dispose()
+	{
+		_isDisposing = true;
+		_enqueueEvent.Set();
+		_thread.Join();
+	}
+
+	public void Start()
+	{
+		_thread.Start();
+	}
+
+	public void EnqueueTask(Action<object?> action, object? parameter, EventWaitHandle? completionEvent = null)
+	{
+		_queue.Enqueue(new ThreadTask(action, parameter, completionEvent));
+		_enqueueEvent.Set();
+	}
+
+	private void ThreadWorker()
+	{
+		while (true)
+		{
+			_enqueueEvent.WaitOne();
+			if (_isDisposing)
+				return;
+
+			while (_queue.TryDequeue(out var task))
+			{
+				task.Execute();
+			}
+		}
+	}
+
+
+	private record ThreadTask(Action<object?> Action, object? Parameter, EventWaitHandle? CompletionEvent)
+	{
+		public void Execute()
+		{
+			try
+			{
+				Action(Parameter);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString()); //TODO: log it properly
+			}
+			finally
+			{
+				CompletionEvent?.Set();
+			}
+		}
+	}
+}
---
+++ Threading/ManagedThreadSynchronizationContext.cs
@@ -0,0 +1,55 @@
+﻿namespace TelegramAIBot.Threading
+{
+	internal sealed class ManagedThreadSynchronizationContext : SynchronizationContext
+	{
+		private readonly ManagedThread _target;
+
+
+		public ManagedThreadSynchronizationContext(ManagedThread target)
+		{
+			_target = target;
+		}
+
+
+		public override SynchronizationContext CreateCopy()
+		{
+			return new ManagedThreadSynchronizationContext(_target);
+		}
+
+		public override void Post(SendOrPostCallback d, object? state)
+		{
+			_target.EnqueueTask(new Action<object?>(d), state);
+		}
+
+		public override void Send(SendOrPostCallback d, object? state)
+		{
+			if (_target.IsInside)
+			{
+				d(state);
+			}
+			else
+			{
+				var resetEvent = new AutoResetEvent(false);
+				_target.EnqueueTask(new Action<object?>(d), state, resetEvent);
+				resetEvent.Set();
+			}
+		}
+
+		public void SendToThread()
+		{
+			Send(action, this);
+
+
+			static void action(object? self)
+			{
+				SetSynchronizationContext((SynchronizationContext)self!);
+			}
+		}
+
+		public static void UseInThread(ManagedThread target)
+		{
+			var context = new ManagedThreadSynchronizationContext(target);
+			context.SendToThread();
+		}
+	}
+}
